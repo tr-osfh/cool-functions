acfe490917b552e76951de2efb1194cd5bbceb9d
Timofei Alekseevich Krasnogorskii <s466342@helios.cs.ifmo.ru>
2026-03-25 01:16:23 +0300

    Merge: combine YAAclass and YAAAclass

--- YAclass.java
+++ YAclass.java
@@ -1,14 +1,15 @@
-
 public class YAclass{
     public static String yaFunc(){
-        return(Yet Another Func)
+        return("Yet Another Func");
     }
 }
-
-
 public class YAAclass{
     public static String yaaFunc(){
         return("Yet Another Another Func");
     }
 }
-
+public class YAAAclass{
+    public static String yaaaFunc(){
+        return("Yet Another Another Another Func");
+    }
+}
